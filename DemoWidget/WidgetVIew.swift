5d2e2f996d10e828578c25663428d7461bbae166
Brooke Hatton <hatton.brooke@gmail.com>
2019-06-14 17:27:39 +0100

    Initial commit

---
+++ DemoWidget/WidgetVIew.swift
@@ -0,0 +1,15 @@
+//
+//  WidgetVIew.swift
+//  DemoWidget
+//
+//  Created by Brooke Hatton on 14/06/2019.
+//  Copyright © 2019 Brooke Hatton. All rights reserved.
+//
+
+import SwiftUI
+
+struct WidgetVIew : View {
+    var body: some View {
+        Text("This is a swiftUI view 👋")
+    }
+}
